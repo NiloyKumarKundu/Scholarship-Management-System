dbb3f31676d61ee2c2eb2ad21ea91c0c088eb9c4
Niloy <nkundu191071@gmail.com>
2021-09-04 17:03:59 +0600

    Scholarship Management System

--- index.php
+++ index.php
@@ -11,6 +11,43 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 
+
+<?php
+if (isset($_POST['submit'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $connection = mysqli_connect("localhost", "root", "", "Scholarship") or die("Not connected" . mysqli_error($connection));
+    
+    $query = "SELECT * FROM users";
+    
+    $result = mysqli_query($connection, $query);
+
+    $count = mysqli_num_rows($result);
+    
+    if ($count > 0) {
+        while ($row = mysqli_fetch_assoc($result)) {
+            // echo "{$row['id']}";
+            // echo "<br>";
+            // echo "{$row['name']}"; 
+            // echo "<br>";
+            // echo "{$row['email']}";
+            // echo "<br>";
+            // echo "{$row['password']}";
+            // echo "<br>";
+            // echo "<br>";
+        }
+        // echo "$count";
+    } else {
+        // echo "You don't have any data in your database!";
+    }
+}
+?>
+
+
+
+
+
 <body class="bag">
 
     <div class="container log">
@@ -22,19 +59,30 @@
                 Enter your necessary information to log in.
             </figcaption>
         </figure>
+        <form action="index.php" method="POST">
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Username" name="username">
+            </div>
 
-        <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-        </div>
+            <div class="mb-3">
+                <input type="password" class="form-control" placeholder="Password" name="password">
+            </div>
 
-        <div class="mb-3">
-            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-        </div>
-        <input type="submit" value="Log In" class="btn btn-primary">
+            <input type="submit" value="Log In" name="submit" class="btn btn-primary">
+            <hr>
+            <div>
+                <p class="footer">
+                    Not registered yet? <a href="./signup.php">Sign Up Here</a>
+                </p>
+            </div>
+        </form>
+    </div>
+
+    
+    <?php require './include/footer.html'; ?>
 </body>
 
 </html>
 
 
 
-<?php require './include/footer.html'; ?>
