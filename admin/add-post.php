8e627b99062a5ac626191419d9384b0acfc1747c
Niloy <nkundu191071@gmail.com>
2021-09-11 10:11:11 +0600

    Project

--- admin/add-post.php
+++ admin/add-post.php
@@ -1,4 +1,5 @@
 <?php include "header.php"; ?>
+
 <div id="admin-content">
     <div class="container">
         <div class="row">
@@ -46,4 +47,5 @@
         </div>
     </div>
 </div>
+
 <?php include "footer.php"; ?>
--- admin/index.php
+++ admin/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>ADMIN | Login</title>
+    <title>Admin | Login</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
 
@@ -20,60 +20,68 @@
 </head>
 
 <body>
-    <div id="wrapper-admin" class="body-content">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-offset-4 col-md-4">
-                    <img class="logo" src="images/new.jpg">
-                    <h3 class="heading">Admin</h3>
-                    <!-- Form Start -->
-                    <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
-                        <div class="form-group">
+
+    <div class="container">
+        <div class="card card-login">
+            <ul class="card-body">
+                <li class="list-group-item">
+                    <h3 >Admin Login</h3>
+                </li>
+            </ul>   
+            <!-- Form Start -->
+            <div class="card-body">
+                <form action="<?php $_SERVER['PHP_SELF'] ?>" method="POST">
+                    <div class="form-group">
+                        <div class="form-label-group">
                             <label>Username</label>
                             <input type="text" name="username" class="form-control" placeholder="" required>
                         </div>
-                        <div class="form-group">
+                    </div>
+
+                    <div class="form-group">
+                        <div class="form-label-group">
                             <label>Password</label>
                             <input type="password" name="password" class="form-control" placeholder="" required>
                         </div>
-                        <input type="submit" name="login" class="btn btn-primary" value="login" />
-                    </form>
-                    <!-- /Form  End -->
-
-                    <?php
-
-                    if (isset($_POST['login'])) {
-                        include "config.php";
-
-                        $username = mysqli_real_escape_string($connection, $_POST['username']);
-                        $password = md5($_POST['password']);
-
-                        $query = "SELECT user_id,username,role FROM users WHERE username='{$username}' AND password='{$password}'";
-                        $result = mysqli_query($connection, $query) or die("Query Failed.");
-
-                        if (mysqli_num_rows($result) > 0) {
-
-                            while ($row = mysqli_fetch_assoc($result)) {
-
-                                session_start();
-
-                                $_SESSION['username'] = $row['username'];
-                                $_SESSION['user_id'] = $row['user_id'];
-                                $_SESSION['user_role'] = $row['role'];
-                                echo $row['username'], $row['user_id'], $row['role'];
-
-                                header("location: post.php");
-                            }
-                        } else {
-                            echo "Username or Password is incorrect.";
-                        }
-                    }
-
-                    ?>
-
-                </div>
+                    </div>
+                    <input type="submit" name="login" class="btn btn-primary btn-block" value="login" />
+                </form>
             </div>
+            <!-- /Form  End -->
+
+            <?php
+
+            if (isset($_POST['login'])) {
+                include "config.php";
+
+                $username = mysqli_real_escape_string($connection, $_POST['username']);
+                $password = md5($_POST['password']);
+
+                $query = "SELECT user_id,username,role FROM users WHERE username='{$username}' AND password='{$password}'";
+                $result = mysqli_query($connection, $query) or die("Query Failed.");
+
+                if (mysqli_num_rows($result) > 0) {
+
+                    while ($row = mysqli_fetch_assoc($result)) {
+
+                        session_start();
+
+                        $_SESSION['username'] = $row['username'];
+                        $_SESSION['user_id'] = $row['user_id'];
+                        $_SESSION['user_role'] = $row['role'];
+                        echo $row['username'], $row['user_id'], $row['role'];
+
+                        header("location: post.php");
+                    }
+                } else {
+                    echo "Username or Password is incorrect.";
+                }
+            }
+
+            ?>
+
         </div>
+
     </div>
 </body>
 
