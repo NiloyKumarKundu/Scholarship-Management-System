17f35156106f1c3c58d0548cfe69a1035c6847a0
Niloy <nkundu191071@gmail.com>
2021-09-18 11:32:39 +0600

    Project

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-        <footer class="bg-light text-center text-lg-start" id="footer">
+        <footer class="bg-light text-center text-lg-start bg-dark text-white">
             <!-- Copyright -->
             <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
                 © 2021 Copyright:
--- header.php
+++ header.php
@@ -11,8 +11,6 @@
     <!-- Bootstrap -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
 
-    <link rel="stylesheet" href="./css/bootstrap.min.css">
-
     <!-- Font Awesome Icon -->
     <link rel="stylesheet" href="./css/font-awesome.css">
 
